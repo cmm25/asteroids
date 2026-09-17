b4318e78e27e0b96a8e0707795761b84107a652d
cmm25 <craigmutegi@gmail.com>
2026-09-17 17:06:58 +0300

    feat: implement asteroid mechanics and update main game loop

--- main.py
+++ main.py
@@ -2,13 +2,29 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+
+    # containers defined
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # containers assigned to classes
+    AsteroidField.containers = updatable
+    Asteroid.containers = (asteroids,updatable, drawable)
+    Player.containers=(updatable,drawable)
+
+    # GAME OBJECTS 
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-            
+    asteroidfield = AsteroidField()
+
+    # GAME LOOP
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,8 +32,9 @@ def main():
                 return
         dt= clock.tick(60)/1000
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)
 
         
 
